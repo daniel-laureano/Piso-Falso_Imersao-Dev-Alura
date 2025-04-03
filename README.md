96bd6a0ee7795bc6fa6fdea942f8a6ed15b5db12
Daniel Laureano <daniellaureanoluz@gmail.com>
2025-04-03 10:57:37 -0300

    Projeto Aula 3 Imersão Dev Alura

---
+++ README.md
@@ -0,0 +1,6 @@
+# A lenda do tesouro perdido - Imersão Dev 
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/daniel-laureano-the-bashful/pen/jEOXgQx](https://codepen.io/daniel-laureano-the-bashful/pen/jEOXgQx).
+
---
+++ src/script.js
@@ -0,0 +1,28 @@
+function jogar() {
+  rodada = 1;
+  chuteFora = 4;
+  while (rodada <= 4) {
+    escolhaJogador = prompt("Tumba " + rodada + " , por onde vai passar? 1-Esquerda 2-Direita 3-Em frente")
+    cantoGoleiro = Math.floor(Math.random() * 3) + 1;  
+    
+    if (escolhaJogador == cantoGoleiro) {
+      alert("Caiu na armadilha! Game Over!");
+      rodada = 6;
+        }
+    
+    if (escolhaJogador >= chuteFora) {
+      alert("Você não pode voltar! Tente novamente.");
+      rodada = 6;
+      break;
+        }
+    
+    if (escolhaJogador != cantoGoleiro) {
+      alert("Isso aí! Voce passou para a próxima tumba!");
+    }
+    
+    rodada = rodada + 1
+    } 
+  if (rodada == 5) {
+      alert("Parabéns! Você encontrou ao tesouro!!! 🎇🏆");
+  }
+}  
